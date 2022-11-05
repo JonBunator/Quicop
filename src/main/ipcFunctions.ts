128d1203bb58941453bd3a4e5b27b22f35f081fe
Jonas B <jonasmitb@gmail.com>
2022-11-05 11:43:21 +0100

    Remove packages, add pdf footer

--- src/main/ipcFunctions.ts
+++ src/main/ipcFunctions.ts
@@ -24,7 +24,7 @@ export default class IpcFunctions {
                 if(pathToPdf == "")
                         pdfPath = path.join(os.homedir(), 'Downloads', 'code_files.pdf')
                         //printBackground: true
-                this.mainWindow?.webContents.printToPDF({pageSize:"A4"}).then(data => {
+                this.mainWindow?.webContents.printToPDF({pageSize:"A4", footerTemplate:"<div><span style=\"font-size:15rem; margin-left:300px;\" class=\"pageNumber\"></span></div>", headerTemplate:" ",displayHeaderFooter:true}).then(data => {
                 fs.writeFile(pdfPath, data, (error) => {
                 if (error) throw error
                 console.log(`Wrote PDF successfully to ${pdfPath}`)
