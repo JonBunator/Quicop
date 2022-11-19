4c89760621ca028a335649c4df4f66a1da06d959
Jonas B <jonasmitb@gmail.com>
2022-11-19 12:29:18 +0100

    Toolbar: Add first draft

--- src/renderer/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/renderer/components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,4 +1,7 @@
-import CodeEditor from '../CodeEditor';
+import IconButton from '@mui/material/IconButton';
+import SyncIcon from '@mui/icons-material/Sync';
+import Tooltip from '@mui/material/Tooltip';
+import CodeEditor from './CodeEditor';
 import FileStatus from '../FileStatus';
 import MarkdownVisualization from '../MarkdownVisualization/MarkdownVisualization';
 import './MarkdownEditor.scss';
@@ -11,11 +14,26 @@ export interface MarkdownEditorProps {
 
 export default function MarkdownEditor(props: MarkdownEditorProps) {
 	const { code, onCodeChange, codeFiles } = props;
+	// const versionString = process.env.npm_package_version;
 	return (
-		<div className="q-editor-panels">
-			<CodeEditor code={code} onChange={onCodeChange} />
-			<div className="q-markdown-panel">
-				<MarkdownVisualization code={code} codeFiles={codeFiles} dark />
+		<div className="q-editor-main-window">
+			<div className="q-editor-panels">
+				<CodeEditor code={code} onChange={onCodeChange} />
+				<div className="q-markdown-panel">
+					<MarkdownVisualization
+						code={code}
+						codeFiles={codeFiles}
+						dark
+					/>
+				</div>
+			</div>
+			<div className="q-editor-footer">
+				<div className="q-editor-footer-version">v1.0.1</div>
+				<Tooltip title="Refresh code files" arrow>
+					<IconButton size="small">
+						<SyncIcon />
+					</IconButton>
+				</Tooltip>
 			</div>
 		</div>
 	);
