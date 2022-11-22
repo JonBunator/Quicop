84f8f4a7b5595f88af27f37fdb018b1c2d14cd6b
Jonas B <62108893+JonBunator@users.noreply.github.com>
2022-11-22 16:20:29 +0100

    Ui: Add light theme, primer components (#5)

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,18 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@primer/react';
 import './App.scss';
 import RootComponent from './components/RootComponent';
+import { GlobalStyle, systemTheme } from './Theme';
 
 export default function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<RootComponent />} />
-			</Routes>
-		</Router>
+		<ThemeProvider theme={systemTheme} colorMode="auto">
+			<GlobalStyle />
+			<Router>
+				<Routes>
+					<Route path="/" element={<RootComponent />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
 	);
 }
